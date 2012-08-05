0bd5d050d9ef8af58f77b767b07d75ddbf78cb5f
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-05 20:14:40 +0200

    update test to use a hydrate function

--- test/bar.js
+++ test/bar.js
@@ -7,19 +7,19 @@ var data = {
   'number':  {count: 10},
   'string':  {drink: 'white russian'},
   'boolean': {isTrue: true},
-  'nested boolean': {theDude: {abides: true, rugCount: 1}}
-  
+  'nested boolean': {theDude: {abides: true, rugCount: 1}}  
 }
 
 
-test('parse(stringify(x)) deepEqual x', function (t) {
+test('parse(stringify(x)) deepEqual x (with hydrate)', function (t) {
+
+  var hydrate = function (str) {
+    try { return JSON.parse(str) } catch (_) {}; return str
+    }
 
   for (var k in data) {
-    //test(k, function (t) {
-      var s = ini.stringify(data[k])
-      console.log(s, data[k])
-      t.deepEqual(ini.parse(s), data[k])
-    //})
+    var s = ini.stringify(data[k])
+    t.deepEqual(ini.parse(s, hydrate), data[k])
   }
 
   t.end() 
