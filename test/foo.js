8ae5a06223807278878589d451e6c13b940a1829
Lloyd Heberlie <lheberlie@esri.com>
2024-07-18 08:20:14 -0500

    fix: remove preserveComments from decode
    ini.js
    foo.js

--- test/foo.js
+++ test/foo.js
@@ -95,7 +95,7 @@ test('encode within browser context', function (t) {
   t.end()
 })
 test('stringify with comments', function (t) {
-  const d = i.parse(data, { preserveComments: true })
+  const d = i.parse(data)
   const s = i.stringify(d, { preserveComments: true })
   t.matchSnapshot(s)
   t.end()
